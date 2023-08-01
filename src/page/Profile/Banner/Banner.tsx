602bb9130b3c09d5f6dd1a2c25763fc68efd63ac
AnatolyVer <tolik.vereshchak88@gmail.com>
2023-08-01 19:54:18 +0300

    Banner changing

--- src/page/Profile/Banner/Banner.tsx
+++ src/page/Profile/Banner/Banner.tsx
@@ -22,28 +22,33 @@ function Banner({ disabled, user }: BannerProps) {
         <div className={`${classes.Banner} ${fixed} ${color}`}>
             {user?.account_banner ? <img className={classes.Image} src={`https://django-auth-gfm6.onrender.com` + user.account_banner} alt="User banner" /> : <></>}
             <div className={classes.User}>
-                <Avatar className={classes.Avatar} alt={user?.nickname.toUpperCase()} src={`https://django-auth-gfm6.onrender.com` +  user?.account_photo}/>
-                <div className={classes.NN}>
-                    <p className={classes.Name}>{user?.username}</p>
-                    <p className={classes.Nickame}>@{user?.nickname}</p>
+                <div className='flex ai-c'>
+                    <Avatar className={classes.Avatar} alt={user?.nickname.toUpperCase()} src={`https://django-auth-gfm6.onrender.com` +  user?.account_photo}/>
+                    <div className={classes.NN}>
+                        <p className={classes.Name}>{user?.username}</p>
+                        <p className={classes.Nickame}>@{user?.nickname}</p>
+                    </div>
                 </div>
-                <div className={classes.Stat}>
-                    <p className={classes.statNumber}>{user?.subscribers_count}</p>
-                    <p className={classes.statName}>підписників</p>
+                <div className='flex ai-c'>
+                    <div className={classes.Stat}>
+                        <p className={classes.statNumber}>{user?.subscribers_count}</p>
+                        <p className={classes.statName}>підписників</p>
+                    </div>
+                    <div className={classes.Stat}>
+                        <p className={classes.statNumber}>{user?.posts_count}</p>
+                        <p className={classes.statName}>публікацій</p>
+                    </div>
+                    <div className={classes.Stat}>
+                        <p className={classes.statNumber}>35</p>
+                        <p className={classes.statName}>фотографій</p>
+                    </div>
                 </div>
-                <div className={classes.Stat}>
-                    <p className={classes.statNumber}>{user?.posts_count}</p>
-                    <p className={classes.statName}>публікацій</p>
+                <div className='flex ai-c'>
+                    <button className={classes.Subscribe}>Підписатися</button>
+                    <EffectButton sx={{ color: "white" }}>
+                        <VertDots sx={{ color: 'white' }} />
+                    </EffectButton>
                 </div>
-                <div className={classes.Stat}>
-                    <p className={classes.statNumber}>35</p>
-                    <p className={classes.statName}>фотографій</p>
-                </div>
-
-                <button className={classes.Subscribe}>Підписатися</button>
-                <EffectButton sx={{ color: "white" }}>
-                    <VertDots sx={{ color: 'white' }} />
-                </EffectButton>
             </div>
         </div>
     );
